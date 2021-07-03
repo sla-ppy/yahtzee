28c904480a5a3257ff91377b71c5113560429f39
sla-ppy <dev@sla-ppy.com>
2021-07-03 07:43:38 +0200

    redesign scoring system, use map for storing rules

--- main.cpp
+++ main.cpp
@@ -1,58 +1,43 @@
 #include <iostream>
 #include <random>
 #include <array>
-#include <sstream>
+#include <map>
 
-int randGen(int max)
-{
+int randGen(int max) {
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(1, max); // Here is where you can change the MIN and MAX values of the random dib.
+    std::uniform_int_distribution<> distrib(1,
+                                            max); // Here is where you can change the MIN and MAX values of the random dib.
 
     int randResult = distrib(gen);
     return randResult;
 }
 
-void lowerSection() {
-    std::cout << '\n';
-    std::cout << "Lower section, WIP" << '\n';
-}
-
-void upperSection() {
+void scoreRolls() {
     // TODO: after watching this video: https://youtu.be/sQM7sN4NPz8 it became evident that my thinking was wrong here is how to continue with the program:
     // TODO: we dont segment upper and lower, they are a whole list.
     // TODO: we first check the lower section to see if we have any specific rolls on there, after each roll
     // TODO: then if we dont, we would either re-roll, or we move to the upper section and add scores there
-    std::stringstream ss{};
 
-    ss << '\n';
-    ss << "Categories:" << '\n';
-    ss << "'1.' Total of Aces(Ones) only" << '\n';
-    ss << "'2.' Total of Two's only" << '\n';
-    ss << "'3.' Total of Threes only" << '\n';
-    ss << "'4.' Total of Fours only" << '\n';
-    ss << "'5.' Total of Fives only" << '\n';
-    ss << "'6.' Total of Sixes only" << '\n';
-    std::cout << ss.str() << '\n';
+    std::map<std::string, const int> scoreMap{{"Total of Aces(ones)", 1},
+                                              {"Total Of Twos",       2},
+                                              {"Total Of Threes",     3},
+                                              {"Total Of Fours",      4},
+                                              {"Total Of Fives",      5},
+                                              {"Total Of Sixes",      6},
+                                              {"3 Of A Kind",         7},
+                                              {"4 Of A Kind",         8},
+                                              {"Full House",          9},
+                                              {"Small Straight",      10},
+                                              {"Large Straight",      11},
+                                              {"YAHTZEE",             12},
+                                              {"Chance",              13}
+    };
 
-    char usrInput{};
-    std::cin >> usrInput;
-
-    if (usrInput == '1') {
-        ss << "1" << '\n';
-    } else if(usrInput == '2') {
-        ss << "2" << '\n';
-    } else if(usrInput == '3') {
-        ss << "3" << '\n';
-    } else if(usrInput == '4') {
-        ss << "4" << '\n';
-    } else if(usrInput == '5') {
-        ss << "5" << '\n';
-    } else if(usrInput == '6') {
-        ss << "6" << '\n';
-    } else {
-        std::cout << "Choose a correct option!" << '\n';
+    for (auto it = scoreMap.begin(); it != scoreMap.end(); it++) {
+        std::cout << it->first << '\n';
     }
+
 }
 
 int main() {
@@ -60,41 +45,30 @@ int main() {
     const int dNumb{5};
     const int rollNumb{3};
 
-    std::array<int, dNumb> dRolls {0};
+    std::array<int, dNumb> dRolls{0};
 
     // 1. Roll dices x3
     for (int i = 0; i != rollNumb; ++i) {
-        std::cout << "Rolls of turn " << i +1 << ":" << '\n';
+        std::cout << "Rolls of turn " << i + 1 << ":" << '\n';
 
         // // 2. Display rolls
         for (auto it = dRolls.begin(); it != dRolls.end(); it++) {
             *it = randGen(6);
-            std::cout << *it << '\n';
+            std::cout << *it << " ";
         }
 
-        std::stringstream ss{};
+        std::cout << '\n';
+        std::cout << "Ready for your scores? Y/N" << '\n';
 
-        bool pickCategory{true};
+        char usrInput{};
+        std::cin >> usrInput;
 
-        ss << '\n';
-        ss << "Choose if you'd like to score by the:" << '\n';
-        ss << "'1.' Upper Section" << '\n';
-        ss << "'2.' Lower Section" << '\n';
-        std::cout << ss.str();
-
-        while (pickCategory)  {
-            char usrInput{};
-            std::cin >> usrInput;
-
-            if(usrInput == '1') {
-                upperSection();
-                pickCategory = false;
-            } else if(usrInput == '2') {
-                lowerSection();
-                pickCategory = false;
-            } else {
-                std::cout << "Choose a correct option!" << '\n';
-            }
+        if (usrInput == 'y' || usrInput == 'Y') {
+            scoreRolls();
+        } else if (usrInput == 'n' || usrInput == 'N') {
+            std::cout << "What about now?" << '\n';
+        } else {
+            std::cout << "Choose a correct option!" << '\n';
         }
     }
 
